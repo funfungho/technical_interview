792efb0558ff32a481dfa71789a0a9288078a1a2
fungho <funfungho@gmail.com>
2023-11-08 23:12:08 -0500

    Two-Stack Algorithm for Expression Evaluation

--- coding/AcWing/expression_evaluation.go
+++ coding/AcWing/expression_evaluation.go
@@ -2,7 +2,17 @@ package main
 
 import "fmt"
 
-// https://www.acwing.com/problem/content/3305/
+/*
+
+Category: Stack
+
+Parentheses are mandatory in this problem, even if they are not necessary.
+
+Dijkstra’s Two-Stack Algorithm for Expression Evaluation
+
+The following is not for this problem: https://www.acwing.com/problem/content/3305/
+
+*/
 
 func main() {
 	operators := []int32{}
@@ -14,7 +24,10 @@ func main() {
 	numChar := []int32{}
 	for _, c := range s {
 		switch c {
-		case '(', ')', '+', '-', '*', '/':
+		// leave out '(' and ' '
+		case '(', ' ':
+			// do nothing
+		case '+', '-', '*', '/':
 			operators = append(operators, c)
 
 			if len(numChar) > 0 {
@@ -25,15 +38,47 @@ func main() {
 				operands = append(operands, num)
 				numChar = []int32{}
 			}
+		case ')':
+			if len(numChar) > 0 {
+				num := 0
+				for _, c := range numChar {
+					num = num*10 + int(c-'0')
+				}
+				operands = append(operands, num)
+				numChar = []int32{}
+			}
+
+			fmt.Println("before", operators, operands)
+
+			op := operators[len(operators)-1]
+			operators = operators[:len(operators)-1]
+
+			valRight := operands[len(operands)-1]
+			valLeft := operands[len(operands)-2]
+			operands = operands[:len(operands)-2]
+
+			switch op {
+			case '+':
+				operands = append(operands, valLeft+valRight)
+			case '-':
+				operands = append(operands, valLeft-valRight)
+			case '*':
+				operands = append(operands, valLeft*valRight)
+			case '/':
+				operands = append(operands, valLeft/valRight)
+			}
+
+			fmt.Println("after", operators, operands)
 		default:
 			numChar = append(numChar, c)
 		}
 	}
-	fmt.Println(operators, operands)
+	fmt.Println(operands[0])
 }
 
 /*
 
-(2+2)*(1+123)
+((2+3)*(4+5))
+(1+((2+3)*(4*5)))
 
 */
