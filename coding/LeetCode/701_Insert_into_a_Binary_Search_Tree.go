a076efd263f79a0cb935f052af15a226d39e0ae8
fungho <funfungho@gmail.com>
2023-12-10 03:04:28 -0500

    one more tree. recursion is a beauty

---
+++ coding/LeetCode/701_Insert_into_a_Binary_Search_Tree.go
@@ -0,0 +1,36 @@
+package main
+
+/*
+
+https://leetcode.com/problems/insert-into-a-binary-search-tree/
+
+Medium, Binary Search Tree
+
+*/
+
+/*
+*
+  - Definition for a binary tree node.
+*/
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func insertIntoBST(root *TreeNode, val int) *TreeNode {
+	if root == nil {
+		return &TreeNode{
+			Val: val,
+		}
+	}
+
+	// * think from leaf node's perspective
+	if root.Val > val {
+		root.Left = insertIntoBST(root.Left, val)
+	} else {
+		root.Right = insertIntoBST(root.Right, val)
+	}
+
+	return root
+}
